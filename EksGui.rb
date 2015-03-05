a524152496d1d4ea13aca996bdc89ec45f87941b
George Pimm <jorjpimm@gmail.com>
2015-03-05 21:53:21 +0000

    disable when qt not available

--- EksGui.rb
+++ EksGui.rb
@@ -1,6 +1,10 @@
 RbMake.import_module('../EksCore')
 RbMake.import_module('../Eks3D')
 
+if (!RbMake.module_exists?(:QtOpenGL))
+  return
+end
+
 RbMake.library(:EksGui, :Eks) do |l, p|
   l.dependencies << :EksCore << :Eks3D << :QtOpenGL
 end 
